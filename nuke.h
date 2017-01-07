84a2a8dc13a8f3cd7b6fc432b21ee42a01095601
BuildTools <unconfigured@null.spigotmc.org>
2017-01-07 04:56:54 +0100

    Debut gestion des projectiles
    obus => projectile
    + obus.h
    + missile.h
    + nuke.h

---
+++ nuke.h
@@ -0,0 +1,12 @@
+#ifndef NUKE_H
+#define NUKE_H
+#include "projectile.h"
+
+class Nuke : public Projectile
+{
+public:
+    Nuke();
+};
+
+
+#endif // NUKE_H
--- obus.h
+++ obus.h
@@ -1,27 +1,4 @@
 #ifndef OBUS_H
 #define OBUS_H
-#include <QPixmap>
-#include <QPoint>
-#include <random>
-#include <QColor>
 
-class Tank
-{
-private:
-    int degats;
-    int radius; //point central + rayon de radius
-    QColor couleur;
-    QPixmap *texture;
-    QPoint impact;
-public:
-    Obstacle();
-    void setDegats(int newDmg);
-    int getRadius();
-    int getDegats();
-    QColor getCouleur();
-    void setCouleur(QColor newCouleur);
-    QPixmap getTexture();
-
-protected:
-    void setTexture(QPixmap *newTexture);
 #endif // OBUS_H
